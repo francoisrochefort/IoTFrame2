0053eb57e4d0fd9d8ff6df241a9333a6a48f807b
Eric Berthiaume <francoisrochefort@hotmail.fr>
2023-10-12 10:02:36 -0400

    commit

--- app/src/main/java/com/etrak/scaleshutdown/ScaleApp.kt
+++ app/src/main/java/com/etrak/scaleshutdown/ScaleApp.kt
@@ -18,15 +18,5 @@ class ScaleApp : Application() {
     override fun onCreate() {
         super.onCreate()
         appModule = AppModuleImpl(this)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                CHANNEL_ID,
-                "Running notifications",
-                NotificationManager.IMPORTANCE_HIGH
-            )
-            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            manager.createNotificationChannel(channel)
-        }
     }
 }
--- app/src/main/java/com/etrak/scaleshutdown/shutdown_service/ShutdownService.kt
+++ app/src/main/java/com/etrak/scaleshutdown/shutdown_service/ShutdownService.kt
@@ -1,6 +1,10 @@
 package com.etrak.scaleshutdown.shutdown_service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.lifecycleScope
@@ -11,17 +15,19 @@ import kotlinx.coroutines.launch
 
 class ShutdownService : LifecycleService() {
 
+    /**********************************************************************************************
+     * Constants
+     *********************************************************************************************/
+
     companion object {
-
         const val DEFAULT_DURATION = 10
-
         const val SHUTDOWN_SEQUENCE_STARTED = "com.example.SHUTDOWN_SEQUENCE_STARTED"
         const val SHUTDOWN_SEQUENCE_COUNTDOWN = "com.example.SHUTDOWN_SEQUENCE_COUNTDOWN"
         const val SHUTDOWN_SEQUENCE_CANCELED = "com.example.SHUTDOWN_SEQUENCE_CANCELED"
         const val EXTRA_DURATION = "com.example.EXTRA_DURATION"
         const val EXTRA_COUNTDOWN = "com.example.EXTRA_COUNTDOWN"
-
-        const val CHANNEL_ID = "running_channel"
+        const val CHANNEL_ID = "state_countdown"
+        const val NOTIFICATION_ID = 1
     }
 
     enum class Action {
@@ -30,74 +36,98 @@ class ShutdownService : LifecycleService() {
         Stop
     }
 
+    /**********************************************************************************************
+     * Variables
+     *********************************************************************************************/
     private lateinit var timer:Timer
     private lateinit var gpio: Gpio
 
-    private val contentTitle: String
-        get() = getString(R.string.content_title)
-    private val contentText: String
-        get() = if (timer.state.value == Timer.State.Stopped)
-            getString(R.string.monitoring_accessory_power)
+    /**********************************************************************************************
+     * Helpers
+     *********************************************************************************************/
+    private fun getContentText(state: Timer.State, countdown: Int? = null) =
+        if (state == Timer.State.Started && countdown != null)
+            String.format(getString(R.string.shutdown_sequence_started), countdown)
         else
-            getString(R.string.shutdown_sequence_started)
+            getString(R.string.monitoring_accessory_power)
+    private fun getContentText() = getContentText(timer.state.value)
 
+    private val builder: NotificationCompat.Builder by lazy {
+        NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.power)
+            .setContentTitle(getString(R.string.content_title))
+            .setContentText(getContentText())
+    }
+
+    private fun updateNotification(state: Timer.State, countdown: Int? = null) {
+        builder.setContentText(getContentText(state, countdown))
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(NOTIFICATION_ID, builder.build())
+    }
+    private fun updateNotification() = updateNotification(timer.state.value)
+
+    /**********************************************************************************************
+     * Command handlers
+     *********************************************************************************************/
     private fun onStart(duration: Int) {
-
         // Init. the timer
         timer = Timer(duration)
-
         // Init. the accessory power gpio
         gpio = Gpio(number = "24")
         gpio.mode = Gpio.Mode.Input
         gpio.registerCallback(
             object : Gpio.GpioCallback {
                 override fun onValueChanged(value: Gpio.State) {
-
                     // If the power is off then start the timer
                     if (value == Gpio.State.Low) timer.start()
-
                     // If the power goes on then stop the timer
                     else if(value == Gpio.State.High) timer.stop()
                 }
             }
         )
-
         // Collect the state of the timer
         lifecycleScope.launch {
             timer.state.collect { state ->
                 when (state) {
-
                     // If the time is up then shutdown the system
                     Timer.State.TimeUp -> {
                         val runtimeObject = Runtime.getRuntime().exec(arrayOf("su", "-c","svc power shutdown"))
                         runtimeObject.waitFor()
                     }
-
                     // If the timer has started then broadcast SHUTDOWN_SEQUENCE_STARTED
-                    Timer.State.Started -> sendBroadcast(Intent(SHUTDOWN_SEQUENCE_STARTED))
-
+                    Timer.State.Started -> {
+                        sendBroadcast(Intent(SHUTDOWN_SEQUENCE_STARTED))
+                        updateNotification()
+                    }
                     // If the timer has stopped then broadcast SHUTDOWN_SEQUENCE_CANCELED
-                    Timer.State.Stopped -> sendBroadcast(Intent(SHUTDOWN_SEQUENCE_CANCELED))
+                    Timer.State.Stopped -> {
+                        sendBroadcast(Intent(SHUTDOWN_SEQUENCE_CANCELED))
+                        updateNotification()
+                    }
                 }
             }
         }
-
         // Collect the countdown of the timer
         lifecycleScope.launch {
             timer.countdown.collect { countdown ->
-
                 // Broadcast the countdown
                 sendBroadcast(Intent(SHUTDOWN_SEQUENCE_COUNTDOWN).putExtra(EXTRA_COUNTDOWN, countdown))
+                updateNotification(timer.state.value, countdown)
             }
+        }
+        // Create the notification channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "Running notifications",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
         }
 
         // Init. the service
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.power)
-            .setContentTitle(contentTitle)
-            .setContentText(contentText)
-            .build()
-        startForeground(1, notification)
+        startForeground(1, builder.build())
     }
 
     private fun onCancelShutdownSequence() {
@@ -116,6 +146,9 @@ class ShutdownService : LifecycleService() {
         return null
     }
 
+    /**********************************************************************************************
+     * Service onStartCommand function
+     *********************************************************************************************/
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
         when (intent?.action) {
